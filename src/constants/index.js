0c2d259e19b722d9ae42ecb197bb18c61e9b6f8a
Neil E. <elkadineil@gmail.com>
2026-06-21 20:09:14 -0400

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,10 @@ import about from "../resources/media/About.jpg";
 
 import neilBot from "../resources/projects/NeilBot.png";
 import valenciaWeb from "../resources/projects/ValenciaTechClub.png";
-import engage from "../resources/projects/UnofficialEngageApi.png";
 import portfolioWebsite from "../resources/projects/Portfolio.png";
-import freelance from "../resources/projects/NeilFreelance.png";
+import kpgpanel from "../resources/projects/kpgpanel.png"
+// import freelance from "../resources/projects/NeilFreelance.png";
+// import engage from "../resources/projects/UnofficialEngageApi.png";
 
 const constants = {
   profile: profile,
@@ -19,9 +20,35 @@ const constants = {
   ],
   experience: [
     {
+      title: "Junior Software Developer",
+      place: "ThreatLocker",
+      time: "August 2025 - Present",
+      skills: [
+        "C#",
+        "ASP.NET",
+        "Typescript",
+        "Angular",
+        "MSSQL",
+      ],
+      description:
+        "Being part of the Rapid Development Team, I am responsible for addressing bugs and introducing small features within 24 hours of assignment.Followiong agile practices, I work through Jira tickets at ~3 tickets per day while ensuring code quality. My responsibility will also include performing peer reviews for others on my team.",
+    },
+    {
+      title: "Application Technecian",
+      place: "ThreatLocker",
+      time: "June 2025 - August 2025",
+      skills: [
+        "HyperV",
+        "Excel Automation",
+        "C# Console Application",
+      ],
+      description:
+        "Though brief, my work as an Application Technician was a valuable experience. Conventionally, the role involves checking over a list of applications and ensuring that we have updated definitions of them. Given my great performance in the position and my technicaly background, I was soon given the opportunity to excercise some programming by automating a task and was promoted soon thereafter.",
+    },
+    {
       title: "Computer Science Tutor",
       place: "Valencia College",
-      time: "August 2023 - Present",
+      time: "January 2023 - June 2025",
       skills: [
         "C",
         "Java",
@@ -31,71 +58,33 @@ const constants = {
         "Teaching",
       ],
       description:
-        "As a tutor, I assist students with various programming courses, leveraging my experience to help with both familiar and unfamiliar subjects. Common areas of assistance include introductory programming, C, Java, advanced courses, C#, scripting languages, computer networking, and Microsoft Office applications.",
-    },
-    {
-      title: "Valencia Tech Club President",
-      place: "Valencia College",
-      time: "January 2023 - Present",
-      skills: [
-        "Leadership",
-        "Web Development",
-        "Public Speaking",
-        "Organizing",
-      ],
-      description:
-        "Leading the Tech Club, I organize technology-related activities such as workshops, tech talks, showcases, and competitions. My role demands strong leadership, organizational skills, and technical proficiency to provide valuable experiences for club members.",
-    },
-    {
-      title: "DevSecOps Administrator",
-      place: "King's Playground",
-      time: "July 2019 - January 2023",
-      skills: [
-        "ASP.NET",
-        "MySQL",
-        "Ubuntu Server",
-        "Nginx",
-        "C# .NET",
-        "SSH",
-        "Shell/Bash",
-      ],
-      description:
-        "At King's Playground, an online multiplayer gaming community, I developed and integrated custom software solutions to enhance game moderation, staff management, and gameplay. Starting as a community leader, I bridged the gap between development and leadership, contributing to the community's success.",
-    },
-    {
-      title: "Web Developer",
-      place: "Freelance",
-      time: "September 2020 - May 2021",
-      skills: [
-        "ReactJS",
-        "ReactBootstrap",
-        "JavaScript",
-        "NodeJS",
-        "Express",
-        "Axios",
-        "Firebase",
-      ],
-      description:
-        "I began my web development journey in a small team, learning and collaborating on projects. We created websites for family and online groups using ReactJS and Bootstrap, managing projects through srcCode and integrating databases with Firebase.",
+        "As a tutor, I assisted students with various programming courses, leveraging my experience to help with both familiar and unfamiliar subjects. Common areas of assistance included introductory programming, C, Java, advanced courses, C#, scripting languages, computer networking, and Microsoft Office applications.",
     },
   ],
   projects: [
+    // {
+    //   title: "Freelance Portal",
+    //   technologies: [
+    //     "NextJS",
+    //     "AuthJS",
+    //     "NodeJS",
+    //     "CSS",
+    //     "MySQL",
+    //     "Prisma",
+    //     "CI/CD Pipelines",
+    //   ],
+    //   srcCode: "https://github.com/Neil925/neil-freelance",
+    //   demo: "https://neilfreelance.com",
+    //   img: freelance,
+    //   description:
+    //     "A personal freelance site that acts as a landing page for the services that I can provide as well as a protal for the submission of freelance requests by users and management of those requests by admins (myself).",
+    // },
     {
-      title: "Freelance Portal",
-      technologies: [
-        "NextJS",
-        "AuthJS",
-        "NodeJS",
-        "CSS",
-        "MySQL",
-        "Prisma",
-        "CI/CD Pipelines",
-      ],
-      srcCode: "https://github.com/Neil925/neil-freelance",
-      demo: "https://neilfreelance.com",
-      img: freelance,
-      description:
-        "A personal freelance site that acts as a landing page for the services that I can provide as well as a protal for the submission of freelance requests by users and management of those requests by admins (myself).",
+      title: "Staff Web Panel",
+      technologies: ["C#", "ASP.NET", "Blazor", "Nginx", "Ubuntu Server", "Docker"],
+      demo: "https://kingsplayground.fun/",
+      img: kpgpanel,
+      description: "A web panel designed to bring in and help staff manage players of an online game. The panel reduced manual work on management by 10 hours per week and increased player retention by upwards of 30%."
     },
     {
       title: "Lesser Server Control",
@@ -114,14 +103,14 @@ const constants = {
       description:
         "A website made by and for Valencia Tech Club. The site presents some general information about the club, its notable members / leaders, and resources on how to become part of the club.",
     },
-    {
-      title: "Unofficial Engage API",
-      technologies: ["NodeJS", "Typescript", "ExpressJS", "Axios", "SQLite"],
-      srcCode: "https://github.com/Neil925/unofficial-engage-api",
-      img: engage,
-      description:
-        "The Unofficial Engage API provides a way to pull event data from the Engage website without having to use Engage's developer API. The API can only show events that are public and accessible to anyone. It accomplishes this by scraping the publicly available data from the Engage website. All information pulled from the Engage website is then stored in a SQLite Database.",
-    },
+    // {
+    //   title: "Unofficial Engage API",
+    //   technologies: ["NodeJS", "Typescript", "ExpressJS", "Axios", "SQLite"],
+    //   srcCode: "https://github.com/Neil925/unofficial-engage-api",
+    //   img: engage,
+    //   description:
+    //     "The Unofficial Engage API provides a way to pull event data from the Engage website without having to use Engage's developer API. The API can only show events that are public and accessible to anyone. It accomplishes this by scraping the publicly available data from the Engage website. All information pulled from the Engage website is then stored in a SQLite Database.",
+    // },
     {
       title: "Portfolio Website",
       technologies: ["ReactJS", "Typescript", "TailWind"],
